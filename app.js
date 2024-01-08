ff2eda690efba96b4cd044642fa6b4d5adb20a02
AndreyV92 <volkovandrey.s92@gmail.com>
2024-01-08 13:10:58 +0700

    ДЗ-Основы

---
+++ app.js
@@ -0,0 +1,8 @@
+const addressLat = 5;
+const addressLong = 10;
+
+const positionLat = 7;
+const positionLong = 14;
+
+
+console.log(Math.sqrt((addressLong - addressLat)**2 + (positionLong - positionLat)**2));
