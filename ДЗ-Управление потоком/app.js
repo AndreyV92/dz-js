e7a2e00a2f3f17d19d27c79ade93c3bd432a7e79
AndreyV92 <volkovandrey.s92@gmail.com>
2024-01-16 16:34:26 +0700

    ДЗ-Управление потоком (отредактированно)
    Убрал лишние переменные

--- ДЗ-Управление потоком/app.js
+++ ДЗ-Управление потоком/app.js
@@ -1,9 +1,5 @@
 const translator = prompt(`На каком языке хотите продолжить общение? ru, en, de`);
 
-const ru = "Привет!";
-const en = "Hello!";
-const de = "guten Tag!";
-
 switch (translator) {
   case "ru":
     console.log("Привет");
